59fb64399c7fe5894e916e7b37ed28e04dc020d6
Sashko <Sashko>
2016-07-31 13:06:35 +0300

    added main files, made search and append to page

---
+++ script.js
@@ -0,0 +1,41 @@
+
+var source = $('#weather-template').html();
+var template = Handlebars.compile(source);
+
+
+var searchCityApi = function (getUserCity){
+  
+  $.get("http://api.openweathermap.org/data/2.5/weather?q="+getUserCity+"&appid=d703871f861842b79c60988ccf3b17ec", function(city){
+    
+    console.log(getUserCity);
+
+
+    var cityName = city.name;   //Get city name from the json
+
+    var tempC = Math.round(city.main.temp - 273.15);          //Convert temp to Celsius
+    var tempF = Math.round(city.main.temp * (9/5) -459.67);   //Convert temp to Fahrenheit
+    var dateTime = new Date();                                //get search date
+    var city = {
+      cityName : cityName,
+      tempC : tempC,
+      tempF : tempF,
+      dateTime : dateTime
+    };
+
+    var newHTML = template(city);
+
+    $('.city-div').append(newHTML);
+  });
+
+    
+
+}
+
+
+$('.btn').on('click', function (e) {
+  e.preventDefault();
+  var getUserCity = $('input').val();
+  // var city = getUserCity.split(" ").join("+");
+  searchCityApi(getUserCity);
+  
+});
