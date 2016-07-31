9c2ba6e98afa736fa6eb40283540342d5ab7719f
Sashko <Sashko>
2016-07-31 16:49:14 +0300

    fixed duplicates on search

--- script.js
+++ script.js
@@ -14,20 +14,29 @@ var saveToLocalStorage = function () {
 
 
 var cities = getFromLocalStorage();
- 
+
 
 var searchCityApi = function (getUserCity){     //search the API for the user's city
   
   $.get("http://api.openweathermap.org/data/2.5/weather?q="+getUserCity+"&appid=d703871f861842b79c60988ccf3b17ec", function(city){
     
-    console.log(getUserCity);
+    // console.log(getUserCity);
 
 
     var cityName = city.name;   //Get city name from the json
 
     var tempC = Math.round(city.main.temp - 273.15);          //Convert temp to Celsius
     var tempF = Math.round(city.main.temp * (9/5) -459.67);   //Convert temp to Fahrenheit
-    var dateTime = new Date();                                //get search date
+                
+
+    var date = new Date();                                    //get seaarch date
+    var day = date.getDate();
+    var monthIndex = 1+date.getMonth();
+    var year = date.getFullYear();    
+    var hours = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    var dateTime = day +"/"+ monthIndex + "/" + year + "    " + hours + ":" + min + ":" + sec;   //Fix the format for date   
 
     var city = {
       cityName : cityName,
@@ -35,14 +44,13 @@ var searchCityApi = function (getUserCity){     //search the API for the user's 
       tempF : tempF,
       dateTime : dateTime
     };
-
+    
     cities.push(city);                       //push the object with the information into the array
-    saveToLocalStorage();
-    console.log(cities);                          //test array
 
-
-    var newHTML = template(city);                 //append
+    $('.city-div').empty();
+    var newHTML = template({cities});                 //append
     $('.city-div').append(newHTML);
+    saveToLocalStorage();
   });
 
 
@@ -54,6 +62,7 @@ function addComment (addCom){
   var $appendCommentDiv = $(addCom).closest('div').closest('.add-comment-div').next('.comments-display');
   $appendCommentDiv.append('<div id = "comm">' + getComment + '</div>');
   // console.log($appendCommentDiv);
+
 };
 
 function deleteCity (deletecity){                   //remove city information (DOM and localStorge)
@@ -67,17 +76,17 @@ function deleteCity (deletecity){                   //remove city information (D
 
 function loadAll(){                 //load all from LocalStorage on page load
   $('city-div').empty();
-  console.log(cities);
+  
   var newHTML = template({cities});                 //append from the array - must use curly braces for handlebars
-  // console.log(newHTML);
-  $('.city-div').append(newHTML);
+   $('.city-div').append(newHTML);
 }
 
 
-$('.btn').on('click', function (e) {
+$('.btn-primary').on('click', function (e) {
   e.preventDefault();
   var getUserCity = $('#city').val();
   // var city = getUserCity.split(" ").join("+");
+
   searchCityApi(getUserCity);
   
 });
@@ -85,6 +94,7 @@ $('.btn').on('click', function (e) {
 $('.city-div'). on ('click', '.btn-success', function(){
   
   addComment(this);
+
  });
 
 $('.city-div').on('click', '.delete-city', function(){
